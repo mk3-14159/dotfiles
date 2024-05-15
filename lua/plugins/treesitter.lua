65721cdb4b37b6080705593d4df1964d3bcc2ef7
mk <evanxmk@gmail.com>
2024-05-15 20:32:05 +0800

    fix: disabling dockerfile highlighting on treesitter helped with the highlighting on Dockerfiles

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,22 +1,22 @@
 return {
-    {
-        -- Autoclose tags in .jsx
-        "windwp/nvim-ts-autotag",
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            local config = require("nvim-treesitter.configs")
-            config.setup({
-                auto_install = true,
-                -- Enable autotag for .jsx f files
-                autotag = {
-                    enable = true,
-                },
-                highlight = { enable = true },
-                indent = { enable = true },
-            })
-        end,
-    },
+	{
+		-- Autoclose tags in .jsx
+		"windwp/nvim-ts-autotag",
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = true,
+				-- Enable autotag for .jsx f files
+				autotag = {
+					enable = true,
+				},
+				highlight = { enable = true, disable = { "dockerfile" } },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
