61916791cd5c5e9a97943f25a99d82e6cdbe4d13
mk <evanxmk@gmail.com>
2024-11-26 12:15:02 +0800

    made dotfiles visible

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,13 +8,35 @@ return {
 	},
 	config = function()
 		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					visible = true, -- when true, they will just be displayed differently than normal items
+					hide_dotfiles = false,
+					hide_gitignored = false,
+					hide_hidden = false, -- only works on Windows for hidden files/directories
+					hide_by_name = {
+						--"node_modules"
+					},
+					hide_by_pattern = { -- uses glob style patterns
+						--"*.meta",
+						--"*/src/*/tsconfig.json",
+					},
+					always_show = { -- remains visible even if other settings would normally hide it
+						--".gitignored",
+					},
+					never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+						--".DS_Store",
+						--"thumbs.db"
+					},
+					never_show_by_pattern = { -- uses glob style patterns
+						--".null-ls_*",
+					},
+				},
+			},
 			event_handlers = {
 				{
 					event = "file_opened",
 					handler = function()
-						-- auto close
-						-- vimc.cmd("Neotree close")
-						-- OR
 						require("neo-tree.command").execute({ action = "close" })
 					end,
 				},
